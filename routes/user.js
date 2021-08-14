d2b8991ee8bc00c281bd7e92f428eda50689e9ef
Thorinwasher <ragnarlol96@gmail.com>
2021-08-14 20:15:28 +0200

    First test with loading images

--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,10 @@ const wrap = require('express-async-handler');
 const e = require('@firstteam102/http-errors');
 const saltRounds = 12;
 
+
+var multer = require('multer');
+var AvatarStorage = require('../helpers/AvatarStorage');
+
 router.all('/*', wrap(async (req, res, next) => {
 	//Must remove from logger context to avoid unwanted persistent funcName.
 	logger.removeContext('funcName');
@@ -186,7 +190,7 @@ router.all('/profile/newserver', wrap(async (req, res, next) => {
 }));
 
 router.get('/profile/newserver', wrap(async (req, res) => {
-	logger.addContext('funcName', 'profile[get]');
+	logger.addContext('funcName', 'newserver[get]');
 	logger.debug('ENTER');
 
 	var user = req.user;
@@ -198,9 +202,53 @@ router.get('/profile/newserver', wrap(async (req, res) => {
 
 }));
 
+//create AvatarStorage object with our own parameters
+var storage = AvatarStorage({
+	square: true,
+	responsive: true,
+	output: "jpg",
+	greyscale: false,
+	quality: 60,
+	threshold: 500
+});
+
+//create image limits (10MB max)
+var limits = {
+	files: 1, // allow only 1 file per request
+	fileSize: 10 * 1024 * 1024, // 10 MB (max file size)
+};
+
+//file filter to guarantee filetype is image
+var fileFilter = function (req, file, cb) {
+
+	//supported image file mimetypes
+	var allowedMimes = ['image/jpeg', 'image/pjpeg', 'image/png', 'image/gif'];
+
+	if (_.includes(allowedMimes, file.mimetype)) {
+		// allow supported image files
+		cb(null, true);
+	} else {
+		// throw error for invalid files
+		cb(new Error('Invalid file type. Only jpg, png and gif image files are allowed.'));
+	}
+};
+
+//create basic multer function upload
+var upload = multer({
+	storage: storage,
+	limits: limits,
+	fileFilter: fileFilter
+}).single("serverpicture");
+
 router.post('/profile/newserver', async function (req, res) {
 	logger.addContext('funcName', 'newserver[post]');
 	logger.debug('ENTER')
+
+	upload(req, res, function (e) {
+		logger.info("Loaded image")
+		logger.debug("image: " + JSON.stringify(req.file))
+		res.redirect('/user/profile')
+    })
 });
 
 router.all('/newpassword', wrap(async (req, res, next) => {
